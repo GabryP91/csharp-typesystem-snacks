45aaa8c187a78eb3b2d10786baa4d30181a7d0cd
Gabriele <gabriele.palma.bool@gmail.com>
2024-04-17 16:23:17 +0200

    add

--- Program.cs
+++ Program.cs
@@ -1,16 +1,13 @@
 ﻿/*
  
 Snack 1
-L’utente inserisce due numeri in successione.
-Il software stampa il maggiore.
+L’utente inserisce due numeri in successione.Il software stampa il maggiore.
 
 Snack 2
-L’utente inserisce due parole in successione.
-Il software stampa prima la parola più corta, poi la parola più lunga.
+L’utente inserisce due parole in successione.Il software stampa prima la parola più corta, poi la parola più lunga.
 
 Snack 3
-Il software deve chiedere per 10 volte all’utente di inserire un numero.
-Il programma stampa la somma di tutti i numeri inseriti.
+Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
 
 Snack 4
 Calcola la somma e la media dei numeri da 2 a 10.
@@ -22,8 +19,7 @@ Snack 6
 In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
 
 Snack 7
-Crea un array vuoto.
-Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+Crea un array vuoto.Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 
 Snack 8
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
@@ -32,9 +28,7 @@ Snack 9
 Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array, fino a quando la somma degli elementi è minore di 50.
 
 Snack 10
-Fai inserire un numero, che chiameremo N, all’utente.
-Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
-Ogni volta che ne crei uno, stampalo a schermo.
+Fai inserire un numero, che chiameremo N, all’utente.Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.Ogni volta che ne crei uno, stampalo a schermo.
  
 */
 
@@ -51,9 +45,9 @@ namespace csharp_typesystem_snacks
     {
         static void Main(string[] args)
         {
-         
+            
             //------------Primo snack--------------------
-            Console.WriteLine("\nPrimo snack:");
+            Console.WriteLine("\nPrimo snack: L’utente inserisce due numeri in successione.Il software stampa il maggiore.");
             Console.WriteLine("\nInserire primo numero:");
             int number1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\nInserire secondo numero:");
@@ -69,7 +63,7 @@ namespace csharp_typesystem_snacks
 
 
             //--------Secondo snack------------------
-            Console.WriteLine("\nSecondo snack:");
+            Console.WriteLine("\nSecondo snack: L’utente inserisce due parole in successione.Il software stampa prima la parola più corta, poi la parola più lunga.");
             Console.WriteLine("\nInserire prima parola:");
             string word1 = Console.ReadLine();
             Console.WriteLine("\nInserire seconda parola:");
@@ -88,6 +82,7 @@ namespace csharp_typesystem_snacks
 
 
             //--------Terzo snack------------------
+            Console.WriteLine("\nTerzo snack: Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.");
             int somma=0;
 
             //Avvio il ciclo 10 volte
@@ -107,8 +102,9 @@ namespace csharp_typesystem_snacks
 
 
             //--------Quarto snack------------------
+            Console.WriteLine("\nQuarto snack: Calcola la somma e la media dei numeri da 2 a 10.");
             somma = 0;
-            int media = 0, numTot=0;
+            int media = 0, numTot = 0;
             for (int i = 2; i <= 10; i++)
             {
                 somma += i; //sommo i valori
@@ -120,21 +116,22 @@ namespace csharp_typesystem_snacks
 
             Console.WriteLine($"\nLa somma Totale sarà: {somma} ");
             Console.WriteLine($"\nLa media sarà: {media} ");
-            
-            //--------Quinto snack------------------
 
+            //--------Quinto snack------------------
+            Console.WriteLine("\nQuinto snack: Il software chiede all’utente di inserire un numero. Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.");
             // Inserisco un numero
             Console.WriteLine("\nInserire un numero:");
-            int numberUser = Convert.ToInt32(Console.ReadLine());
+            int numberUser2 = Convert.ToInt32(Console.ReadLine());
 
-            if (numberUser % 2 == 0)
+            if (numberUser2 % 2 == 0)
             {
-                Console.WriteLine($"\nIl numero è: {numberUser} ed è pari ");
+                Console.WriteLine($"\nIl numero è: {numberUser2} ed è pari ");
             }
             else
-                Console.WriteLine($"Il numero inserito è dispari, il successivo è :{numberUser + 1}");
+                Console.WriteLine($"Il numero inserito è dispari, il successivo è :{numberUser2 + 1}");
 
             //--------Sesto snack------------------
+            Console.WriteLine("\nSesto snack: In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.");
             string[] Invitati = { "pippo", "pluto", "paperino" };
 
             Console.WriteLine("\nInserire il tuo nome:");
@@ -150,6 +147,107 @@ namespace csharp_typesystem_snacks
             }
 
             //--------Settimo snack------------------
+            Console.WriteLine("\nSettimo snack: Crea un array vuoto.Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.");
+            int[] arrayDisp = new int[0];
+
+            int contatore = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                // Inserisco un numero
+                Console.WriteLine("\nInserire un numero:");
+                int numberUser3 = Convert.ToInt32(Console.ReadLine());
+
+                // Verifico se il numero inserito è dispari
+                if (numberUser3 % 2 != 0)
+                {
+                    
+                    // ridimensiono il mio array vuoto con una nuova posizione
+                    Array.Resize(ref arrayDisp, arrayDisp.Length + 1);
+
+                    //assegno il valore digitato dall'utente all'ultima posizione creata
+                    arrayDisp[arrayDisp.Length - 1] = numberUser3;
+
+                    contatore++; // Incremento il contatore
+                }
+
+            }
+            
+            // Stampo i numeri dispari inseriti nell'array
+            Console.WriteLine("Numeri dispari inseriti:");
+            for (int i = 0; i < contatore; i++)
+            {
+                Console.WriteLine(arrayDisp[i]);
+            }
+
+            //--------Ottavo snack------------------
+            Console.WriteLine("\nOttavo snack: Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.");
+            int[] ArrayInt = new int[] {1,2,3,4,5,6,88,45,13,24,11,51,347};
+
+            int sommaValDisp = 0;
+
+            //modifico il ciclo for in modo tale da "beccare" solo i valori nelle posizioni dispari
+            for (int i = 1; i < ArrayInt.Length; i += 2)
+            {
+                //Sommo tranquillamente ogni valore che ottengo dall'array
+                sommaValDisp += ArrayInt[i];
+            }
+
+            Console.WriteLine($"\nla somma totale di tutti gli elementi in posizione dispari sarà: {sommaValDisp}");
+
+
+            //--------Nono snack------------------
+            Console.WriteLine("\nNono snack: Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array, fino a quando la somma degli elementi è minore di 50.");
+            // Creo il mio array vuoto
+            int[] ArrayGen = new int[0];
+
+            // Variabile per memorizzare la somma degli elementi
+            somma = 0;
+
+            while (somma<50)
+            {
+                // Inserisco un numero
+                Console.WriteLine("\nInserire un numero:");
+                int numberUser4 = Convert.ToInt32(Console.ReadLine());
+
+              
+                // ridimensiono il mio array vuoto con una nuova posizione
+                Array.Resize(ref ArrayGen, ArrayGen.Length + 1);
+
+                //assegno il valore digitato dall'utente all'ultima posizione creata
+                ArrayGen[ArrayGen.Length - 1] = numberUser4;
+
+
+                // Aggiorna la somma degli elementi
+                somma += numberUser4;
+            }
+
+            Console.WriteLine($"\nla somma dei numeri inseriti è maggiore di 50");
+
+            //--------Decimo snack------------------
+            Console.WriteLine("\nDecimo snack: Fai inserire un numero, che chiameremo N, all’utente.Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.Ogni volta che ne crei uno, stampalo a schermo.");
+            Console.WriteLine("Digitare valore di N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            //inizializzo un oggetto random
+            Random random = new Random();
+
+            for (int i= 0; i<N;i++)
+            {
+                //per N Volte genero un array di grandezza 10
+                int[] arrayN = new int[10];
+
+                Console.WriteLine($"array {i+1}");
+
+                for (int j=0; j<arrayN.Length;j++)
+                {
+                    arrayN[j] = random.Next(1, 101);
+                    Console.WriteLine($"\n valore:{arrayN[j]} ");
+                }
+
+                Console.WriteLine(); // Vai a capo per il prossimo array
+            }
+
 
             Console.ReadKey();
         }
